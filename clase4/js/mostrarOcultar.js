42b4fa2b7227ca902a78bd55c1df9b8d4a072109
Marcos <exegeses@gmail.com>
2022-02-24 20:32:28 -0300

    resolución tarea mostrar/ocultar

--- clase4/js/mostrarOcultar.js
+++ clase4/js/mostrarOcultar.js
@@ -12,3 +12,36 @@ function mostrar()
     caja.style.display = 'block';
     //caja.style.visibility = 'visible';
 }
+/*
+//solución Martín Vicente
+function mostrarOcultar()
+{
+    if( caja.style.display == 'block' )
+    //if( caja.style.visibility == 'visible' )
+    {
+        ocultar();
+    }
+    else
+    {
+        mostrar();
+    }
+}
+*/
+// solución Cami Lázaro
+let view = true;//flag booleano
+function mostrarOcultar() {
+    // si hace click ocultamos
+    // si vuelve a hacer click mostramos
+    if ( view ) {
+        ocultar();
+        //caja.style.visibility ='hidden';
+        //view = false;
+    }
+    else {
+        mostrar();
+        //caja.style.visibility ='visible';
+        //view = true;
+    }
+    view = !view; // si es true, le pongo false
+    // si es false, le pongo true
+};
